8589a6a6bc84776592aaeafe4c57420bb5b9aa47
kennedyifeadi <kennedyifeadi@gmail.com>
2024-02-23 22:41:38 +0100

    initail code base

---
+++ app.js
@@ -0,0 +1,17 @@
+// const entryAge = 225;
+
+// if (entryAge < 18) {
+//     console.log("nice you are of age, wlecome to smash the party");
+// } else{
+//     console.log("we are sorry you are not of age");
+// }
+// const voterAge =17;
+const voterAge = 2;
+
+if (voterAge >= 18) {
+    console.log("You are of age. head right to voting center, and please remember to always be with your pvc");
+    
+} else{
+    console.log("we are sorry, but you are not of age");
+}
+console.log("blah blah bllah");
